15908a869c60ff4ffc28edca067c119b592a9782
윤병일 <byoungilyoun@yunbyeong-il-ui-MacBookPro-2.local>
2023-06-02 14:02:56 +0900

    Setup Project

---
+++ TCA-Counter/TCA-Counter/TCA_CounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCA_CounterApp.swift
+//  TCA-Counter
+//
+//  Created by 윤병일 on 2023/06/02.
+//
+
+import SwiftUI
+
+@main
+struct TCA_CounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TCA-Counter/TCA-CounterUITests/TCA_CounterUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  TCA_CounterUITestsLaunchTests.swift
+//  TCA-CounterUITests
+//
+//  Created by 윤병일 on 2023/06/02.
+//
+
+import XCTest
+
+final class TCA_CounterUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
