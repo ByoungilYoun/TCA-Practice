70deb64245ac0b21b308d6fe224973d9f36fbdd9
윤병일 <byoungilyoun@yunbyeong-il-ui-MacBookPro-2.local>
2023-06-02 14:11:19 +0900

    TCA package dependency 추가

---
+++ TCA-Counter/TCA-Counter/CounterView.swift
@@ -0,0 +1,21 @@
+//
+//  CounterView.swift
+//  TCA-Counter
+//
+//  Created by 윤병일 on 2023/06/02.
+//
+
+import SwiftUI
+import ComposableArchitecture
+
+struct CounterView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CounterView_Previews: PreviewProvider {
+    static var previews: some View {
+        CounterView()
+    }
+}
